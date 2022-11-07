3429a67e8f4fbbd50849db9fd06a58f2ee995188
EldinMesic <heallios88@gmail.com>
2022-11-07 17:28:25 +0100

    Task 4 - Extracting Functionality into a Service Layer

--- EM_WebApplication_RUAP-LV2/EM_WebApplication_RUAP-LV2/Controllers/ContactController.cs
+++ EM_WebApplication_RUAP-LV2/EM_WebApplication_RUAP-LV2/Controllers/ContactController.cs
@@ -5,26 +5,22 @@ using System.Linq;
 using System.Net;
 using System.Net.Http;
 using System.Web.Http;
+using EM_WebApplication_RUAP_LV2.Services;
 
 namespace EM_WebApplication_RUAP_LV2.Controllers
 {
     public class ContactController : ApiController
     {
+        private ContactRepository contactRepository;
+
+        public ContactController()
+        {
+            this.contactRepository = new ContactRepository();
+        }
+
         public Contact[] Get()
         {
-            return new Contact[]
-            {
-                new Contact
-                {
-                    Id = 1,
-                    Name = "Glenn Block"
-                },
-                new Contact
-                {
-                    Id = 2,
-                    Name = "Dan Roth"
-                }
-            };
+            return contactRepository.GetAllContacts();
         }
     }
 }
